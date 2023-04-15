21ac302395a992ecdf847f3e74b16382a4bfa96d
Artem <113770041+Artemdream@users.noreply.github.com>
2023-04-15 14:51:43 +0300

    app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,12 @@
 import { lazy } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { Layout } from '../Layout/Layout';
+import { Layout } from './Layout/Layout';
 
 const Home = lazy(() => import('pages/Home'));
 const Movies = lazy(() => import('pages/Movies'));
-const MovieDetails = lazy(() => import('../MovieDetails/MovieDetails'));
-const Cast = lazy(() => import('../Cast/Cast'));
-const MovieReviews = lazy(() => import('../Reviews/Reviews'));
+const MovieDetails = lazy(() => import('./MovieDetails/MovieDetails'));
+const Cast = lazy(() => import('./Cast/Cast'));
+const MovieReviews = lazy(() => import('./Reviews/Reviews'));
 
 export const App = () => {
   return (
